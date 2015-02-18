e1e312e4e777bedf50288ce57b24ec88398839c9
Sanjay B <b@pad.ma>
2015-02-18 12:08:09 +0530

    minor, fix static file dir path

--- aksharaklp/aksharaklp/settings.py
+++ aksharaklp/aksharaklp/settings.py
@@ -83,7 +83,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static"
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'templates'),
+    os.path.join(PROJECT_ROOT, '../templates'),
 )
 
 # List of finder classes that know how to find static files in
